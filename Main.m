5ff938179fb0492d88132319ccac1b49e4795cc1
kbcovingtonjr <kbcovingtonjr@gmail.com>
2016-12-03 23:49:13 -0700

    Working on graphing Cl ad Cd vs. angle at 10 m/s

--- Main.m
+++ Main.m
@@ -9,15 +9,19 @@ clear all; close all; clc;
 
 %% Definitions
 
-% The Pressure Ports coordinates in % Chord
-x = [0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 80, 70, 60, 50, 40, 30, 20, 10, 5];
-y = [4.19, 9.45, 11.48, 13.6, 14, 13.64, 12.58, 10.95, 8.8, 6.25, 0, 0, 0, 0, 0, 0, 0.04, 0.5, 1.11];
-
 c = 3.5 * 0.0254; % chord length [m]
 
-% % Outline the pressure ports that are being skipped
-% xpp_skip = [70, 70, 50];
-% ypp_skip = [8.8, 0, 0];
+% The Pressure Ports coordinates in % Chord
+x = [0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 80, 70, 60, 50, 40, 30, 20,...
+    10, 5];
+y = [4.19, 9.45, 11.48, 13.6, 14, 13.64, 12.58, 10.95, 8.8, 6.25, 0, 0,...
+    0, 0, 0, 0, 0.04, 0.5, 1.11];
+
+skipPositions = [9, 12, 14]; % positions with no pressure data taken
+
+% Remove skipped positions from x and y vectors
+x(skipPositions) = [];
+y(skipPositions) = [];
 
 % Scale the Profile for the chord length given
 x_scaled = c*x/100;
@@ -31,7 +35,8 @@ data20 = data{2};   % data collected at 20 m/s
 data30 = data{3};   % data collected at 30 m/s
 
 % this will change to be a for loop of 1:3 to use the data from each speed
-% ...only doing this for air speed of 10 m/s now
+% ...only doing this for air speed of 10 m/s now--will be scaled once
+% functional
 
 Patm = data10(:,1);           % atmospheric pressure [Pa]
 Tatm = data10(:,2);           % atmospheric temperature [K]
@@ -40,56 +45,63 @@ speed = data10(:,4);          % air speed in test section [m/s]
 Ppitot = data10(:,5);         % dynamic pressure [Pa]
 Paux = data10(:,6);           % uhhhhhhhhhh
 presPorts = data10(:,7:22);   % pressures at each port [Pa]
-angle = data10(:,23);         % angle of attack [degrees]
+angleDeg = data10(:,23);         % angle of attack [degrees]
 normF = data10(:,24);         % uhhhhhhhhhh
 axialF = data10(:,25);        % uhhhhhhhhhh
 moment = data10(:,26);        % uhhhhhhhhhh
 ELD_X = data10(:,27);         % uhhhhhhhhhh
 ELD_Y= data10(:,28);          % uhhhhhhhhhh
 
+angleR = angleDeg*pi/180;
 
 %% Calculations
 
 % Calculate Cp values
 Cp1 = findCP(presPorts, Patm, Ppitot);
-Cp2 = findCP(circshift(presPorts,-1,2), circshift(Patm,-1),...
-    circshift(Ppitot,-1));
+Cp2 = findCP(circshift(presPorts,-1,2), Patm, Ppitot);
 
 % Resize some shtuff to work for Cn and Ca calculations - (something is
 % going wrong here)
 [rows, cols] = size(presPorts);
-x1 = repmat(x_scaled,cols,1);
+x1 = repmat(x_scaled,rows,1);
 x2 = circshift(x1,-1,2);
-y1 = repmat(y_scaled,cols,1);
-y2 = circshift(y_scaled,-1,2);
-c = repmat(c,cols,19);
+y1 = repmat(y_scaled,rows,1);
+y2 = circshift(y1,-1,2);
+% c = repmat(c,rows,cols);
 
-% Testing sizes of matrices
-[rows, cols] = size(x1)
-[rows, cols] = size(y1)
-[rows, cols] = size(c)
-[rows, cols] = size(Cp1)
+% Testing sizes of matrices - (becuase something is going wrong here)
+% size(presPorts)
+% size(Cp1)
+size(x1)
+size(y1)
+size(x2)
+size(y2)
+% size(c)
 
 % Cn and Ca calculations
 Cn = findCn(Cp1, Cp2, x1, x2, c);
 Ca = findCa(Cp1, Cp2, y1, y2, c);
 
-% Cn and Ca calculations
-Cl = Cn .* cos(angle) - Ca .* sin(angle);
-Cd = Cn .* sin(angle) - Ca .* cos(angle);
+% Cl and Cd calculations
+Cl = Cn .* cos(angleR) - Ca .* sin(angleR);
+Cd = Cn .* sin(angleR) + Ca .* cos(angleR);
 
 
 %% Plot data
 
 % Plot Cl and Cd
-figure;
-hold on;
-plot(angle,Cl)
-plot(angle,Cd)
+figure
+title('Lift and Drag Coefficients');
 
+subplot(1,2,1);
+plot(angleDeg,Cl,'-*');
+xlabel('Angle of Attack (degrees)');
+ylabel('Cl');
 
-%% Error analysis
-
+subplot(1,2,2);
+plot(angleDeg,Cd,'-*');
+xlabel('Angle of Attack (degrees)');
+ylabel('Cd');
 
 
 
